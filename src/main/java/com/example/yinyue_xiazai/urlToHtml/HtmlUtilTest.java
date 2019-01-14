f557a99b37d8557902db87eda9a85b37dcbf2bf9
段 <1021567205@qq.com>
2019-01-14 23:44:36 +0800

    家提交

--- src/main/java/com/example/yinyue_xiazai/urlToHtml/HtmlUtilTest.java
+++ src/main/java/com/example/yinyue_xiazai/urlToHtml/HtmlUtilTest.java
@@ -1,14 +1,9 @@
 package com.example.yinyue_xiazai.urlToHtml;
 import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.CookieManager;
 import com.gargoylesoftware.htmlunit.NicelyResynchronizingAjaxController;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.junit.Test;
-
-import java.util.List;
 
 public class HtmlUtilTest {
 	
@@ -16,21 +11,30 @@ public class HtmlUtilTest {
 	public static void main(String[] args) {
 		
     	
-    	String url = "http://www.190757.com/";
-    	
-    	
-    	
+    	String url = "http://www.djye.com/";
     	
     	
         final WebClient webClient = new WebClient(BrowserVersion.CHROME);//新建一个模拟谷歌Chrome浏览器的浏览器客户端对象
 
-        webClient.getOptions().setThrowExceptionOnScriptError(false);//当JS执行出错的时候是否抛出异常, 这里选择不需要
-        webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);//当HTTP的状态非200时是否抛出异常, 这里选择不需要
+        
+    	//启动js
+    	webClient.getOptions().setJavaScriptEnabled(true);
+    	//关闭css渲染
+    	webClient.getOptions().setCssEnabled(false);
+    	//启动重定向
+    	webClient.getOptions().setRedirectEnabled(true);
+    	//启动cookie管理
+    	webClient.setCookieManager(new CookieManager());
+    	//启动ajax代理
+    	webClient.setAjaxController(new NicelyResynchronizingAjaxController());
+    	//js运行时错误，是否抛出异常
+    	webClient.getOptions().setThrowExceptionOnScriptError(false);
+    	
+        webClient.getOptions().setThrowExceptionOnFailingStatusCode(true);//当HTTP的状态非200时是否抛出异常, 这里选择不需要
         webClient.getOptions().setActiveXNative(false);
-        webClient.getOptions().setCssEnabled(false);//是否启用CSS, 因为不需要展现页面, 所以不需要启用
-        webClient.getOptions().setJavaScriptEnabled(true); //很重要，启用JS
-        webClient.setAjaxController(new NicelyResynchronizingAjaxController());//很重要，设置支持AJAX
-
+        
+        
+        
         HtmlPage page = null;
         try {
             page = webClient.getPage(url);//尝试加载上面图片例子给出的网页
@@ -40,7 +44,7 @@ public class HtmlUtilTest {
             webClient.close();
         }
 
-        webClient.waitForBackgroundJavaScript(30000);//异步JS执行需要耗时,所以这里线程要阻塞30秒,等待异步JS执行结束
+        webClient.waitForBackgroundJavaScript(300000);//异步JS执行需要耗时,所以这里线程要阻塞30秒,等待异步JS执行结束
 
         String pageXml = page.asXml();//直接将加载完成的页面转换成xml格式的字符串
         
